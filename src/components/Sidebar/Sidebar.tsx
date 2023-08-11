efb9ba8cb015c0177e7c86dfbc491c3e0e7d144e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-11 15:39:06 +0100

    Added sidenav component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface SidebarProps {
+    
+}
+
+const Sidebar:React.FC = (): JSX.Element => {
+
+    return (
+        <React.Fragment>
+            <h1>Sidebar works!</h1>
+        </React.Fragment>
+    );  
+}
+
+export default Sidebar;
+    
