8269f5fa9646eaf878955aa0777e81d19f3dcc16
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-11 15:17:56 +0100

    Extended tailwind to project color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend:{
+      colors:{
+        green:{
+          "100" : "#4BD291",
+          "200" : "#3BCE87",
+          "300" : "#2EB875",
+          "400" : "#2DB472",
+          "500" : "#29A368",
+          "600" : "#25935E",
+          "700" : "#218353",
+          "800" : "#1D7249",
+          "900" : "#18623E"
+        }
+      }
+    }
   },
   plugins: [],
-}
-
+};
