f06bb50613a71536d339cbd9f1059fa2a205a287
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-14 09:44:53 +0100

    Added ScanItem component

---
+++ src/components/ScanItem/ScanItem.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface ScanItemProps {
+    
+}
+
+const ScanItem:React.FC = (): JSX.Element => {
+
+    return (
+        <React.Fragment>
+            <h1>ScanItem works!</h1>
+        </React.Fragment>
+    );  
+}
+
+export default ScanItem;
+    
