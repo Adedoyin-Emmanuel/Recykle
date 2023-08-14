44bf4aa99615fdd5f2d1fcc7aed6e900abdb8148
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-14 13:54:06 +0100

    Added ScanItemContainer

---
+++ src/components/ScanItemContainer/ScanItemContainer.tsx
@@ -0,0 +1,18 @@
+
+import React from "react";
+
+interface ScanItemContainerProps {
+    
+}
+
+const ScanItemContainer:React.FC = (): JSX.Element => {
+
+    return (
+        <React.Fragment>
+            <h1>ScanItemContainer works!</h1>
+        </React.Fragment>
+    );  
+}
+
+export default ScanItemContainer;
+    
