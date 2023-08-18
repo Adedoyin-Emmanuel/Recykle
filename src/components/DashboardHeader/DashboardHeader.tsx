bd3a2ada8769476fb0a270e7d6f511ee17576a96
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-18 10:02:08 +0100

    Updated Dashboard header little components

--- src/components/DashboardHeader/DashboardHeader.tsx
+++ src/components/DashboardHeader/DashboardHeader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-regular-svg-icons";
 
@@ -10,6 +10,56 @@ const DashboardHeader: React.FC = ({
   className,
   ...others
 }: DashboardHeaderProps): JSX.Element => {
+  const [isNotificationDropdownVisible, setIsNotificationDropdownVisible] =
+    useState(false);
+  const [isProfileDropdownVisible, setIsProfileDropdownVisible] =
+    useState(false);
+
+  const notificationRef = useRef(null); // Reference to the notification dropdown
+  const profileRef = useRef(null); // Reference to the profile dropdown
+
+  useEffect(() => {
+    const closeDropdowns = (event: MouseEvent) => {
+      if (
+        (isNotificationDropdownVisible &&
+          notificationRef.current &&
+          !notificationRef.current.contains(event.target as Node)) ||
+        (isProfileDropdownVisible &&
+          profileRef.current &&
+          !profileRef.current.contains(event.target as Node))
+      ) {
+        setIsNotificationDropdownVisible(false);
+        setIsProfileDropdownVisible(false);
+      }
+    };
+
+    window.addEventListener("click", closeDropdowns);
+
+    return () => {
+      window.removeEventListener("click", closeDropdowns);
+    };
+  }, [isNotificationDropdownVisible, isProfileDropdownVisible]);
+
+  const toggleNotificationDropdown = () => {
+    setIsNotificationDropdownVisible(!isNotificationDropdownVisible);
+  };
+
+  const toggleProfileDropdown = () => {
+    setIsProfileDropdownVisible(!isProfileDropdownVisible);
+  };
+
+  const notificationItems = [
+    { id: 1, text: "New message from admin" },
+    { id: 2, text: "Reminder: Meeting at 3 PM" },
+    { id: 3, text: "Your submission has been approved" },
+  ];
+
+  const profileMenuItems = [
+    { id: 1, text: "Edit Profile" },
+    { id: 2, text: "Change Password" },
+    { id: 3, text: "Logout" },
+  ];
+
   return (
     <section
       className={`dashboard-header w-full flex items-center justify-between p-2 mb-3 ${className}`}
@@ -22,16 +72,60 @@ const DashboardHeader: React.FC = ({
       </section>
 
       <section className="action-icons flex items-center justify-around gap-x-5">
-        <section className="notification cursor-pointer">
+        <section
+          className="notification cursor-pointer relative"
+          ref={notificationRef}
+        >
           <FontAwesomeIcon
             icon={faBell}
             size={"lg"}
             className="transition-colors transform-gpu hover:bg-slate-100 p-3 shadow-sm rounded-full hover:text-green-300"
+            onClick={toggleNotificationDropdown}
           />
+
+          <section
+            className="notification-numbers absolute top-7 left-4 bg-green-10 flex items-center justify-center p-3 h-5 w-9 rounded-full"
+            onClick={toggleNotificationDropdown}
+          >
+            <p className="notification-number text-[11px] font-bold ">10+</p>
+          </section>
+          {isNotificationDropdownVisible && (
+            <div className="notification-dropdown absolute  top-full w-72 right-0 bg-white z-[100] rounded-md shadow-md p-4">
+              <h4 className="mb-2 font-bold text-gray-800 capitalize">
+                Notifications
+              </h4>
+              {notificationItems.map((item) => (
+                <p
+                  key={item.id}
+                  className="text-sm p-2 hover:bg-green-10 rounded"
+                >
+                  {item.text}
+                </p>
+              ))}
+            </div>
+          )}
         </section>
 
-        <section className="profile cursor-pointer">
-          <section className="bg-green-300 h-10 w-10 rounded-full"></section>
+        <section className="profile-image cursor-pointer relative">
+          <section
+            className="bg-green-300 h-10 w-10 rounded-full"
+            onClick={toggleProfileDropdown}
+          ></section>
+          {isProfileDropdownVisible && (
+            <div className="notification-dropdown absolute  top-full w-72 right-0 bg-white z-[100] rounded-md shadow-md p-4">
+              <h4 className="mb-2 font-bold text-gray-800 capitalize">
+                Profile Menu
+              </h4>
+              {profileMenuItems.map((item) => (
+                <p
+                  key={item.id}
+                  className="text-sm p-2 hover:bg-green-10 rounded"
+                >
+                  {item.text}
+                </p>
+              ))}
+            </div>
+          )}
         </section>
       </section>
     </section>
