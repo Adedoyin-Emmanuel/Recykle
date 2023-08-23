399aa3b6ad643025c04dd971b69eb9cee0f92130
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-08-23 09:10:20 +0100

    Updated the company app and auth contexts

---
+++ src/context/companyAppContext.tsx
@@ -0,0 +1,189 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { createContext, useContext, useEffect, useState } from "react";
+import { useCompanyAuth, CompanyAuthContextProps } from "./companyAuthContext";
+import {
+  addDoc,
+  doc,
+  collection,
+  serverTimestamp,
+  updateDoc,
+  increment,
+  getDocs,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "../utils/firebase.config";
+import Notification from "../utils/toast";
+import { User as FirebaseUser } from "firebase/auth";
+
+interface CompanyAppContextProps {
+  children?: React.ReactNode;
+}
+
+export interface CompanyAppContextValuesProps {
+  username: string;
+  company: FirebaseUser | any;
+  companyContextLoading: boolean;
+  companyLoading: boolean;
+  companyData: any;
+  appContextError: any;
+  addServiceItem: (
+    companyId: string,
+    serviceDetails: {
+      serviceName: string;
+      serviceCategory: string;
+    }
+  ) => any;
+  getCompanyServiceCollection: (companyId: string) => any;
+  updateCompanyServiceItem: (
+    companyId: string,
+    itemId: string,
+    updatedItemDetails: any
+  ) => void;
+  deleteCompanyServiceItem: (companyId: string, itemId: string) => void;
+}
+
+export const AppContext = createContext<CompanyAppContextValuesProps>({
+  username: "",
+  company: null,
+  companyContextLoading: true,
+  companyLoading: true,
+  companyData: {},
+  appContextError: {},
+  addServiceItem: async () => null,
+  getCompanyServiceCollection: async () => null,
+  updateCompanyServiceItem: async () => null,
+  deleteCompanyServiceItem: async () => null,
+});
+
+export const AppContextProvider = ({ children }: CompanyAppContextProps) => {
+  const { company, getDocumentData, companyLoading }: CompanyAuthContextProps =
+    useCompanyAuth();
+
+  const [appContextLoading, setAppContextLoading] = useState<boolean>(true);
+  const [companyData, setCompanyData] = useState<any>({});
+  const [appContextError, setAppContextError] = useState<string | null>(null);
+  const toast = new Notification();
+
+  useEffect(() => {
+    if (!companyLoading && company) {
+      getDocumentData("users", company.uid)
+        .then((documentData: any) => {
+          if (documentData) {
+            setCompanyData(documentData);
+            setAppContextLoading(false);
+            setAppContextError(null);
+          } else {
+            console.log("Document not found");
+            setAppContextLoading(false);
+          }
+        })
+        .catch((error: Error) => {
+          console.error(error);
+          setAppContextLoading(false);
+          setAppContextError("An error occurred while fetching user data.");
+        });
+    }
+  }, [companyLoading, company]);
+
+  const addServiceItem = async (companyId: string, serviceDetails: any) => {
+    try {
+      const companyDocRef = doc(db, "companies", companyId);
+      const itemsCollectionRef = collection(companyDocRef, "serviceCollections");
+
+      await addDoc(itemsCollectionRef, {
+        itemName: serviceDetails.serviceName,
+        category: serviceDetails.serviceCategory,
+        dateAdded: serverTimestamp(),
+      });
+
+      await updateDoc(companyDocRef, {
+        totalItemsAdded: increment(1),
+      });
+
+      console.log("Service added successfully");
+      toast.success("Service added successfully");
+      return true;
+    } catch (error) {
+      console.error("Error adding recycling item:", error);
+      toast.error("Error adding service");
+      return false;
+    }
+  };
+
+  const getCompanyServiceCollection = async (companyId: string) => {
+    try {
+      const companyDocRef = doc(db, "companies", companyId);
+      const collectionRef = collection(companyDocRef, "itemsCollections");
+
+      const querySnapshot = await getDocs(collectionRef);
+
+      const services = querySnapshot.docs.map((doc) => {
+        const data = doc.data();
+        return {
+          name: data.serviceName,
+          category: data.serviceCategory,
+          dateAdded: data.dateAdded.toDate().toLocaleDateString(),
+        };
+      });
+
+      return services;
+    } catch (error) {
+      console.error("Error getting service items:", error);
+      return [];
+    }
+  };
+
+  const updateServiceItem = async (
+    companyId: string,
+    serviceId: string,
+    updatedItemDetails: any
+  ) => {
+    try {
+      const companyDocRef = doc(db, "companies", companyId);
+      const collectionRef = collection(companyDocRef, "collections");
+      const serviceDocRef = doc(collectionRef, serviceId);
+
+      await updateDoc(serviceDocRef, updatedItemDetails);
+
+      toast.success("Service item updated successfully");
+    } catch (error) {
+      console.error("Error updating recycling item:", error);
+      toast.error("Error updating service item");
+    }
+  };
+
+  const deleteServiceItem = async (companyId: string, serviceId: string) => {
+    try {
+      const companyDocRef = doc(db, "companies", companyId);
+      const collectionRef = collection(companyDocRef, "collections");
+      const serviceDocRef = doc(collectionRef, serviceId);
+
+      await deleteDoc(serviceDocRef);
+
+      console.log("Recycling item deleted successfully");
+    } catch (error) {
+      console.error("Error deleting recycling item:", error);
+    }
+  };
+
+  const value: CompanyAppContextValuesProps = {
+    username: companyData.username,
+    company: company,
+    companyContextLoading: appContextLoading,
+    companyLoading: companyLoading,
+    companyData: companyData,
+    appContextError: appContextError,
+    addServiceItem: addServiceItem,
+    getCompanyServiceCollection: getCompanyServiceCollection,
+    updateCompanyServiceItem: updateServiceItem,
+    deleteCompanyServiceItem: deleteServiceItem,
+  };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
---
+++ src/context/companyAuthContext.tsx
@@ -0,0 +1,314 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createContext, useContext, useState, useEffect } from "react";
+import Notification from "../utils/toast";
+import { auth, db } from "../utils/firebase.config";
+import {
+  doc,
+  setDoc,
+  GeoPoint,
+  query,
+  collection,
+  where,
+  getDocs,
+  getDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signOut,
+  signInWithPopup,
+  User as FirebaseUser,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+interface companyAuthProps {
+  children: React.ReactNode;
+}
+
+export interface CompanyAuthContextProps {
+  company: FirebaseUser | null;
+  companyLoading: boolean;
+  isAuthenticated: boolean;
+  loginWithCredentials: (email: string, password: string) => Promise<boolean>;
+  loginWithGoogleAccount: () => Promise<boolean>;
+  registerWithCredentials: (
+    email: string,
+    password: string,
+    fullname: string,
+    username: string
+  ) => Promise<boolean>;
+  registerWithGoogleAccount: () => Promise<boolean>;
+  logout: () => void;
+  updateCompanyLocation: (
+    company: FirebaseUser,
+    latitude: number,
+    longitude: number
+  ) => Promise<boolean>;
+  getUsername: (user: any) => Promise<string | null>;
+  getDocumentData: (
+    collectionName: string,
+    documentId: string
+  ) => Promise<any | null>;
+}
+
+const CompanyAuthContext = createContext<CompanyAuthContextProps>({
+  company: null,
+  companyLoading: true,
+  isAuthenticated: false,
+  loginWithCredentials: async () => false,
+  loginWithGoogleAccount: async () => false,
+  registerWithCredentials: async () => false,
+  registerWithGoogleAccount: async () => false,
+  logout: () => {},
+  updateCompanyLocation: async () => false,
+  getUsername: async () => null,
+  getDocumentData: async () => null,
+});
+
+export const CompanyAuthProvider = ({ children }: companyAuthProps) => {
+  const [company, setCompany] = useState<any>(null);
+  const [companyLoading, setCompanyLoading] = useState<boolean>(true);
+  const googleProvider = new GoogleAuthProvider();
+  const toast = new Notification(3000);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCompany(user);
+      setCompanyLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, [auth]);
+
+  const loginWithCredentials = async (email: string, password: string) => {
+    try {
+      const companyCredentials = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      setCompany(companyCredentials.user);
+      toast.success("Login successful");
+      return true;
+    } catch (error) {
+      console.error(error);
+      toast.error("Login error");
+      return false;
+    }
+  };
+
+  const loginWithGoogleAccount = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const newCompany = result.user;
+
+      // Check if the user's email is already registered
+      const existingUserQuerySnapshot = await getDocs(
+        query(collection(db, "companies"), where("email", "==", newCompany.email))
+      );
+
+      if (!existingUserQuerySnapshot.empty) {
+        setCompany(newCompany); // Corrected this line
+        toast.success("Login successful");
+        return true;
+      } else {
+        // User's email is not registered, show an error message
+        toast.error("This email is not registered. Please sign up.");
+        logout();
+        return false;
+      }
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
+
+  const isAuthenticated = !!company; // Determine whether user is authenticated
+
+  const registerWithCredentials = async (
+    email: string,
+    password: string,
+    fullname: string,
+    username: string
+  ) => {
+    try {
+      const usernameTaken = await checkIfUsernameIsTaken(username);
+
+      if (!usernameTaken) {
+        const companyCredentials = await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+        await setDoc(doc(db, "companies", companyCredentials.user.uid), {
+          fullname: fullname,
+          username: username,
+          email: email,
+          dateCreated: serverTimestamp(),
+          itemsRecycled: 0,
+          profileImageUrl: "",
+          role: "company",
+          itemsReceived: 0,
+          verified: false,
+          lastLogin: serverTimestamp(),
+          usersFeedback: 0,
+        });
+        setCompany(companyCredentials.user);
+        toast.success("Company registered successfully");
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error: any) {
+      console.error(error);
+      toast.error("Signup error");
+      return false;
+    }
+  };
+
+  const registerWithGoogleAccount = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      const user = result.user;
+
+      await setDoc(doc(db, "companies", user.uid), {
+        fullname: user.displayName,
+        username: user.displayName?.toLowerCase().split(" ").join(""),
+        email: user.email,
+        dateCreated: serverTimestamp(),
+        usersFeedback: 0,
+        profileImageUrl: user.photoURL,
+        role: "company",
+        itemsReceived: 0,
+        verified: false,
+        lastLogin: serverTimestamp(),
+        itemsRecycled: 0,
+      });
+
+      setCompany(user);
+      toast.success("company registered successfully");
+      return true;
+    } catch (error) {
+      console.error(error);
+      toast.error("Signup error");
+      return false;
+    }
+  };
+
+  const checkIfUsernameIsTaken = async (username: string) => {
+    if (!username) return "errror with username";
+
+    try {
+      const querySnapshot = await getDocs(
+        query(collection(db, "companies"), where("username", "==", username))
+      );
+
+      return !querySnapshot.empty;
+    } catch (error) {
+      console.error("Error checking username:", error);
+      return error; // Return true if an error occurs to be cautious
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      setCompany(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateCompanyLocation = async (
+    company: FirebaseUser,
+    latitude: number,
+    longitude: number
+  ): Promise<boolean> => {
+    try {
+      if (!company || !latitude || !longitude) {
+        console.log(company);
+        toast.error("Invalid user or location data");
+        return false;
+      }
+
+      const location = new GeoPoint(latitude, longitude);
+      await setDoc(doc(db, "companies", company.uid), { location }, { merge: true });
+      toast.success("Location saved");
+      return true;
+    } catch (error) {
+      console.error(error);
+      toast.error("Error saving location");
+      return false;
+    }
+  };
+
+  const getUsername = async (user: any) => {
+    if (!user) return;
+
+    try {
+      const { uid }: string | any = user;
+      const userRef = collection(db, "companies");
+      const q = query(userRef, where("userId", "==", uid));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        const userDoc = querySnapshot.docs[0];
+        return userDoc.data().username;
+      } else {
+        return null; // No company found with the given company
+      }
+    } catch (error) {
+      console.error("Error getting username:", error);
+      return null;
+    }
+  };
+
+  const getDocumentData = async (
+    collectionName: string,
+    documentId: string
+  ) => {
+    try {
+      const documentRef = doc(db, collectionName, documentId);
+      const documentSnapshot = await getDoc(documentRef);
+
+      if (documentSnapshot.exists()) {
+        const documentData = documentSnapshot.data();
+        return documentData;
+      } else {
+        return null; // Document not found
+      }
+    } catch (error) {
+      console.error("Error getting document data:", error);
+      return null;
+    }
+  };
+
+  const value: CompanyAuthContextProps = {
+    company: company,
+    companyLoading: companyLoading,
+    isAuthenticated,
+    updateCompanyLocation: updateCompanyLocation,
+    loginWithCredentials,
+    loginWithGoogleAccount,
+    registerWithCredentials,
+    registerWithGoogleAccount,
+    logout,
+    getUsername,
+    getDocumentData,
+  };
+
+  return (
+    <CompanyAuthContext.Provider value={value}>
+      {children}
+    </CompanyAuthContext.Provider>
+  );
+};
+
+export const useCompanyAuth = () => {
+  return useContext(CompanyAuthContext);
+};
