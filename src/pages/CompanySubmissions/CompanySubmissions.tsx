f2a8005002177699ffc09ab3a3e9cddbbce366ed
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2023-09-13 20:29:34 +0100

    Bug fix

--- src/pages/CompanySubmissions/CompanySubmissions.tsx
+++ src/pages/CompanySubmissions/CompanySubmissions.tsx
@@ -1,19 +1,13 @@
-
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+// import {Link, useNavigate} from "react-router-dom";
 
-interface CompanySubmissionsProps {
-    
-}
-
-const CompanySubmissions:React.FC = (): JSX.Element => {
-    const navigateTo = useNavigate();
-    return (
-        <React.Fragment>
-            <h1>CompanySubmissions works!</h1>
-        </React.Fragment>
-    );  
-}
+const CompanySubmissions: React.FC = (): JSX.Element => {
+  //const navigateTo = useNavigate();
+  return (
+    <React.Fragment>
+      <h1>CompanySubmissions works!</h1>
+    </React.Fragment>
+  );
+};
 
 export default CompanySubmissions;
-    
